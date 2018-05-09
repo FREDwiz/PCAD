6df1704f138ab9e9e6e65ec679b33fb059fce967
Federico <fumbrella@gmail.com>
2018-05-09 17:53:43 +0200

    almost
    - removed useless files
    - updated gitignore

--- Lab5_ServerJava/Client.java
+++ Lab5_ServerJava/Client.java
@@ -1,8 +1,9 @@
 /*
 *   Client
-*   I client si collegano al server nviando una richiesta di registrazione con il proprio user-id (stringa).
-*   Se lo user-id compare già nella lista il server risponde al client con "Fail".
-*   Altrimenti aggiunge lo user-id alla lista e risponde con "Ok".
+*   I client si collegano al server nviando una richiesta di registrazione con il proprio user-id (stringa).	X
+*   Se lo user-id compare già nella lista il server risponde al client con "Fail".								X
+*   Altrimenti aggiunge lo user-id alla lista																	X
+*	e risponde con "Ok".																						X
 */
 
 
@@ -11,19 +12,26 @@ import java.net.*;
 import java.util.UUID;
 
 class TCPClient {
- public static void main(String argv[]) throws Exception {
+	public static void main(String argv[]) throws Exception {
 
-    String uuid = UUID.randomUUID().toString();
-    outToServer.writeBytes(uuid + '\n');
+		String uuid = UUID.randomUUID().toString();
 
-      String response;
-      //BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-      Socket clientSocket = new Socket("localhost", 9000);
-      DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-      BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      
-      response = inFromServer.readLine();
-      System.out.println("FROM SERVER: " + response);
-      //clientSocket.close();
- }
+		String response;
+
+		Socket clientSocket = new Socket("localhost", 9000);
+
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+		outToServer.writeBytes(uuid + '\n');
+
+		response = inFromServer.readLine();
+
+		if(response.equals("CONNECTED")) { System.out.println(response + ": Connection accepted"); }
+		else{ System.out.println(response + ": Connection refused"); }
+
+		System.out.println("Closing socket");
+		clientSocket.close();
+	}
 }
--- Lab5_ServerJava/Server.java
+++ Lab5_ServerJava/Server.java
@@ -1,51 +1,66 @@
 /*
 *	Server
-*	Il Server usa ServerSocket sulla porta 9000 per accettare connessioni TCP da client e un ThreadPool
-*	con 5 thread per gestire ogni richiesta di un client attraverso un socket dedicato.
-*	Il server mantiene in una lista in memoria gli user-id di utenti registrati.
-*	Utilizzare un timer (o un timeout sul accept) per chiudere  il SeverSocket e stampare la lista di utenti registrati.
-*/
-
-/*
-
-if (IDs.contains(uuid)) {
-    System.out.println("uuid found");
-} else {
-    System.out.println("uuid not found");
-}
-
-
-List<String> IDs = new ArrayList<String>();
-IDs.add(uuid);
-
+*	Il Server usa ServerSocket sulla porta 9000 per accettare connessioni TCP da client 						X
+*	e un ThreadPool con 5 thread per gestire ogni richiesta di un client attraverso un socket dedicato.			TODO
+*	Il server mantiene in una lista in memoria gli user-id di utenti registrati.								X
+*	Utilizzare un timer (o un timeout sul accept) per chiudere  il SeverSocket 									X
+*	e stampare la lista di utenti registrati.																	X
 */
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
+
 
 class TCPServer {
- public static void main(String argv[]) throws Exception {
-  String uuid;
-  String capitalizedUUID;
-  ServerSocket welcomeSocket = new ServerSocket(9000);
+	public static void main(String argv[]) throws Exception {
+		
+		final int PORT = 9000;
+		final int TIMEOUT = 60000;
+		static int numberOfThreads = 5;
 
-  List<String> IDs = new ArrayList<String>();
+		String uuid;
+		ServerSocket welcomeSocket = new ServerSocket(PORT);
+		
 
-	if (IDs.contains(uuid)) {
-	    System.out.println("Connection refused! You're already connected");
-	} else {
-		Socket connectionSocket = welcomeSocket.accept();
-   		BufferedReader inFromClient =
-    		new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-   		DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-   		uuid = inFromClient.readLine();
-   		System.out.println("Welcome " + uuid);
-   		capitalizedUUID = uuid.toUpperCase() + '\n';
-   		outToClient.writeBytes(capitalizedUUID);
+
+		System.out.println("TCPServer Waiting for client on port " + PORT);
+		
+		List<String> IDs = new ArrayList<String>();
+
+		while (true) {
+
+			System.out.println("Waiting for new connections: Timeout in " + TIMEOUT/1000 + " seconds");
+
+			welcomeSocket.setSoTimeout(TIMEOUT);
+		   
+			Socket connectionSocket = welcomeSocket.accept();
+			BufferedReader inFromClient =
+				new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+			uuid = inFromClient.readLine();
+			if(!IDs.contains(uuid)){
+				
+				outToClient.writeBytes("CONNECTED");
+				outToClient.close();
+
+				IDs.add(uuid);
+
+				System.out.println("Guess who joined:\n" + uuid);
+
+				System.out.println("\nOther joiners:");
+
+				for (int i = 0; i < IDs.size(); i++) { System.out.println(IDs.get(i)); }
+					
+			}
+			else{ 
+				
+				System.out.println(uuid + " Already connected");
+				outToClient.writeBytes("FAIL");
+				outToClient.close();
+			}
+		}
 	}
- }
 }
-
-
-
-
