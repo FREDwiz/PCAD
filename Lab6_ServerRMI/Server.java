1b59026c1f5242289e9af4ff785ce51cc8970a90
Federico <fumbrella@gmail.com>
2018-05-14 18:24:06 +0200

    server lab6

--- Lab6_ServerRMI/Server.java
+++ Lab6_ServerRMI/Server.java
@@ -2,3 +2,14 @@
 *	Server
 *	Il Server deve esportare un oggetto remoto con due metodi remoti
 */
+
+
+import java.rmi.*;
+
+public interface Server extends java.rmi.Remote{
+	String sayHello() throws RemoteException;
+}
+
+public static void main(String[] args) {
+	
+}
